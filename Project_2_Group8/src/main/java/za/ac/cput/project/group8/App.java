59948410df13e862df99b5f89462b65d2e0e8a59
Gilberto <Gilberto@macs-mbp.home>
2021-04-01 09:04:39 +0200

    Third commit

--- Project_2_Group8/src/main/java/za/ac/cput/project/group8/App.java
+++ Project_2_Group8/src/main/java/za/ac/cput/project/group8/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       int result= add(8, 9);
+        System.out.println("Hello World!" + result);
+        
     }
+    public static int add(int num1, int num2){
+
+        return num1 + num2;
+}
+    
 }
--- Project_2_Group8/src/test/java/za/ac/cput/project/group8/AppTest.java
+++ Project_2_Group8/src/test/java/za/ac/cput/project/group8/AppTest.java
@@ -1,20 +1,69 @@
 package za.ac.cput.project.group8;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+//import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        //Assert.that(true, "dhhhdgbnc");  
+                
+    }
+
+    /**
+     * Test of main method, of class App.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of add method, of class App.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int num1 = 0;
+        int num2 = 0;
+        int expResult = 0;
+        int result = App.add(num1, num2);
+       // assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    //     fail("The test case is a prototype.");
     }
 }
